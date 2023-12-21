d62eefbe0540ccbe4b6820a4172df313df04c5f0
gibberish <lamgiabao1456@gmail.com>
2023-12-21 15:45:28 +0700

    Added rpc.lua game script

---
+++ rpc.lua
@@ -0,0 +1,33 @@
+-- usage:
+-- LoadScript("custom/rpc.lua")
+-- SendRPCData(GetRPCData())
+function SendRPCData(jsondata)
+    -- this doesnt work for now
+    TheSim:QueryServer(
+        "0.0.0.0:1974/api/rotwoodrpc",
+        nil,
+        "POST",
+        jsondata
+    )
+end
+
+function GetRPCData()
+    local payload = {}
+    payload["ingame"] = InGamePlay()
+    payload["localgame"] = TheNet:IsGameTypeLocal()
+    payload["playercount"] = #AllPlayers
+    if (TheWorld) then
+        payload["room"] = TheWorld.prefab
+    else
+        payload["room"] = ""
+    end
+    if (TheDungeon) then
+        payload["biome"] = TheDungeon:GetDungeonMap():GetBiomeLocation().pretty.name
+    else
+        payload["biome"] = ""
+    end
+
+    local payload_json = json.encode(payload)
+
+    return payload_json
+end
